9404079c2c51a84031c11100912dca000dabe28b
nguyen quang <quangnd1211@gmail.com>
2023-04-15 09:07:33 +0700

    init source

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Admin123
+export POSTGRES_HOST=db-project-3.cufgdneagofy.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project_3
+export AWS_BUCKET=quangnd20-336596164563-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
